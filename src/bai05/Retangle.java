06362204183eed8d65bec38d982e23e75a64e076
Tam <tamanhtran473@gmail.com>
2022-12-18 22:49:13 +0700

    bai05

---
+++ src/bai05/Retangle.java
@@ -0,0 +1,52 @@
+package bai05;
+
+public class Retangle  extends Shape {
+    private double width;
+    private double length;
+    public Retangle(){}
+
+    public Retangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Retangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea()
+    {
+        return  this.length * this.width;
+    }
+
+    public  double getPerimeter()
+    {
+        return 2* (this.length + this.width);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                "} " + super.toString();
+    }
+}
---
+++ src/bai05/Squares.java
@@ -0,0 +1,43 @@
+package bai05;
+
+
+    public class Squares extends Retangle{
+
+        private double side;
+    
+        public Squares(){}
+    
+        public Squares(double side) {
+            this.side = side;
+        }
+    
+        public Squares(double color, double filled, double side) {
+            super(color, filled);
+            this.side = side;
+        }
+    
+        public double getSide() {
+            return side;
+        }
+    
+        public void setSide(double side) {
+            this.side = side;
+        }
+    
+        public void setWidth()
+        {
+            setWidth(this.side);
+        }
+    
+        public void setLength()
+        {
+            setLength(this.side);
+        }
+    
+        @Override
+        public String toString() {
+            return "Squares{" +
+                    "side=" + side +
+                    "} " + super.toString();
+        }
+}
